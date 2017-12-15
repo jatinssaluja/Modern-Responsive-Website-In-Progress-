3383a409020605e9f7b7e4aa476af251f094f2f9
jatinsinghsaluja <jatin_humane@ymail.com>
2017-12-14 19:55:23 -0500

    Added Work Section

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
     new WOW().init();
     
 });
+
+
+$(document).ready(function() {
+  $('#work').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery:{
+    enabled:true
+  }
+  });
+});
